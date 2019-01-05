b03653f65950f432ccee0531ee2d1f119d1c14a5
Cory Steers <developer@madsteer.com>
2019-01-04 19:26:34 -0600

    added haters are hating

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,19 @@
 import Routing
 import Vapor
+import Leaf
 
 /// Register your application's routes here.
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("hello") { req -> Future<View> in
+        struct ExampleData: Codable {
+            let haters = "hating"
+            let names = ["Taylor", "Paul", "Justin", "Adele"]
+            let results = ["Scott": 90, "Dan": 85, "Liz": 100]
+            let quote = "He thrusts his fists against the posts and still insists he sees the ghosts"
+        }
+
+        return try req.view().render("home", ExampleData())
     }
 }
