3f066fadcc68d309dc5fc5b7cfe8c2d31c1138af
Cory Steers <developer@madsteer.com>
2019-01-05 23:19:50 -0600

    finish zebra striping the unordered list

--- Sources/App/Zebra.swift
+++ Sources/App/Zebra.swift
@@ -13,6 +13,8 @@ public final class ZebraTag: TagRenderer {
     public func render(tag parsed: TagContext) throws -> Future<TemplateData> {
         try parsed.requireParameterCount(3)
 
+        let prefix = "<div class=\"row\"><div class=\"col-md-3\">"
+        let suffix = "</li></div></div>"
         return Future.map(on: parsed.container) {
             if let name = parsed.parameters[0].string,
                 let index = parsed.parameters[1].int,
@@ -26,8 +28,7 @@ public final class ZebraTag: TagRenderer {
                 }
 
                 let fixedText = text.replacingOccurrences(of: "INDEX", with: String(index)).replacingOccurrences(of: "NAME", with: name)
-//                var finalText = myLiTag + fixedText + "</li>"
-                return .string(myLiTag + fixedText + "</li>")
+                return .string(prefix + myLiTag + fixedText + suffix)
 
             } else {
                 return .null
